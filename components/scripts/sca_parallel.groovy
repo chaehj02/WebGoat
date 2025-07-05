39de49ee6f6578ef921736cd8c477a236a9c1c9f
JEOng_ <kara10041@gmail.com>
2025-07-05 19:03:32 +0900

    Update sca_parallel.groovy

--- components/scripts/sca_parallel.groovy
+++ components/scripts/sca_parallel.groovy
@@ -2,10 +2,12 @@ def call() {
     def jobs = [:]
 
     for (int i = 1; i <= 2; i++) {
-        def index = i  // 클로저 내 변수 캡처 방지
+        def index = i
         jobs["SCA-Run-${index}"] = {
-            stage("SCA-Run-${index}") {
-                sh "/home/ec2-user/run_sbom_pipeline.sh"
+            node("SCA-agent${index}") {
+                stage("SCA-Run-${index}") {
+                    sh "/home/ec2-user/run_sbom_pipeline.sh"
+                }
             }
         }
     }
