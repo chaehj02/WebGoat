b9e67340c04839815f657ff4cac281ce444f45c7
JEOng_ <kara10041@gmail.com>
2025-07-05 20:44:56 +0900

    Update sca_parallel.groovy

--- components/scripts/sca_parallel.groovy
+++ components/scripts/sca_parallel.groovy
@@ -1,18 +1,16 @@
-def call() {
-    def jobs = [:]
+echo "✅ SCA 병렬 실행 시작"
 
-    for (int i = 1; i <= 2; i++) {
-        def index = i
-        jobs["SCA-Run-${index}"] = {
-            node("SCA-agent${index}") {
-                stage("SCA-Run-${index}") {
-                    sh "bash \$WORKSPACE/components/scripts/run_sbom_pipeline.sh ..."
-                }
+def jobs = [:]
+
+for (int i = 1; i <= 2; i++) {
+    def index = i
+    jobs["SCA-Run-${index}"] = {
+        node("SCA-agent${index}") {
+            stage("SCA-Run-${index}") {
+                sh "bash \$WORKSPACE/WebGoat/components/scripts/run_sbom_pipeline.sh https://github.com/WH-Hourglass/WebGoat.git WebGoat ${index}"
             }
         }
     }
-
-    parallel jobs
 }
 
-call()
+parallel jobs
