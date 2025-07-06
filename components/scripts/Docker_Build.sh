f46cce3fc2e0f98f68486d5bf25516583805d5cc
Watermelonlatte <rnjs0332@naver.com>
2025-07-06 21:52:07 +0900

    이미지태그 수정

--- components/scripts/Docker_Build.sh
+++ components/scripts/Docker_Build.sh
@@ -1,3 +1,10 @@
 #!/bin/bash
+#source components/dot.env
+#docker build -t $ECR_REPO:$IMAGE_TAG .
+
+#!/bin/bash
+set -o allexport
 source components/dot.env
-docker build -t $ECR_REPO:$IMAGE_TAG .
+set +o allexport
+
+docker build -t "$ECR_REPO:$IMAGE_TAG" .
